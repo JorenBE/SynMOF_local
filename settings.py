f9d4e70d306b8c14afce9f48f59faeb00ab71fb5
Joren <joren.vanherck@epfl.ch>
2025-06-10 17:03:53 +0200

    create test case

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
-FOLDER = '/home/jorenvanherck/Documents/synmofWeb/local_loop/PRISMA'
+FOLDER = '/home/jorenvanherck/Documents/synmofWeb/local_loop/test'
 
-FOLDER_CALCULATED = '/home/jorenvanherck/Documents/synmofWeb/local_loop/calculated_PRISMA'
+FOLDER_CALCULATED = '/home/jorenvanherck/Documents/synmofWeb/local_loop/test_calculated'
 if not os.path.exists(FOLDER_CALCULATED):
     os.mkdir(FOLDER_CALCULATED)
